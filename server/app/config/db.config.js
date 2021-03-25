597b7f236f2744ad4f00ca89fedbc5df8276f2f9
sobeki <filipe_cf35@hotmail.com>
2021-03-25 16:19:27 -0300

    att db cfg

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,8 +1,8 @@
 export default  {
   HOST: "localhost",
-  USER: "root",
-  PASSWORD: "root",
-  DB: "backpack",
+  USER: "",
+  PASSWORD: "",
+  DB: "",
   dialect: "mysql",
   pool: {
     max: 5,
