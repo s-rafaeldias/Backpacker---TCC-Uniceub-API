35fd870be639a190de71274a3147c44c0a785977
Rafael Dias <rafa197795@gmail.com>
2021-03-27 16:02:19 -0300

    feat(auth): cria esqueleto do middleware

--- server/app/middlewares/authMiddleware.js
+++ server/app/middlewares/authMiddleware.js
@@ -1,32 +1,30 @@
 import firebase from "@firebase/app";
 import * as admin from 'firebase-admin';
 import firebaseConfig from '../config/firebase.js'
-import "@firebase/auth";
+// import "@firebase/auth";
+// import "firebase";
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = ''
 admin.default.initializeApp(firebaseConfig);
 
-const auth = async (req,res,next) => {
+const auth = async (req, res, next) => {
     try {
-        //teste
-        // let user = await admin
-        //     .default
-        //     .auth()
-        //     .createCustomToken('CCYpQPh1GOYGGziTq44zIpKKZyB3')
-        // console.log(user);
-        // let user = await admin.default.auth().getUser('CCYpQPh1GOYGGziTq44zIpKKZyB3')
-        // console.log(user)
-        let decodedToken = await admin.default.auth().verifyIdToken(idToken)
-        console.log(decodedToken);
-        
+        let token = await admin
+            .auth()
+            .verifyIdToken(req.body.token);
+
+        let userID = token.uid;
+        let emailVerified = token.email_verified
+
+        if (emailVerified) {
+            // TODO: fazer paranaue no banco de marcar email como verificado
+            next();
+        }
+
+        res.status(401).json({'msg': 'Email nao verificado'});
     } catch (error) {
         console.log(error);
-    }
-
-    if('email' in req.body){
-        next()
-    }else{
-        res.status(500).json({'msg':'asdd'})
+        res.status(401).json({'msg': 'Token invalido'});
     }
 };
 
