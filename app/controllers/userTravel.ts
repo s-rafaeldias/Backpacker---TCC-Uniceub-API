db8bccbbd8cf08b234308b10b16e4a5c36c10f8f
sobeki <filipe_cf35@hotmail.com>
2021-04-15 20:58:15 -0300

    Rotas e controle ( user-travel )

---
+++ app/controllers/userTravel.ts
@@ -0,0 +1,74 @@
+import { createNewTraveler, deleteUserTravel, getUsersFromTravel} from "../services/userTravel";
+import { Request, Response } from "express";
+import { UniqueConstraintError, ValidationError } from "sequelize";
+
+const UserTravelController = {
+  create: async (req: Request, res: Response) => {
+    try {
+      await createNewTraveler(req);
+      return res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+
+      if (err instanceof UniqueConstraintError) {
+        return res.status(400).json({
+          message: "Campo chave duplicado",
+          status: "Failure",
+        });
+      } else if (err instanceof ValidationError) {
+        return res.status(400).json({
+          message: err.message,
+          status: "Failure",
+        });
+      }
+
+      return res.status(500).json({
+        message: "Erro",
+        status: "Failure",
+      });
+    }
+  },
+
+  getDetails: async (req: Request, res: Response) => {
+    try {
+      let { id_viagem } = req.params;
+      
+      let users = await getUsersFromTravel(id_viagem);
+
+      if (users !== null) {
+        return res.status(200).json(users)
+      }
+
+      return res.status(404).json({
+        message: "Not Found",
+      });
+    } catch (err) {
+      console.log(err);
+
+      return res.status(500).json({
+        message: "Bad Request",
+      });
+    }
+  },
+
+  delete: async (req: Request, res: Response) => {
+
+    try {
+      let { id_usuario, id_viagem } = req.params;
+      await deleteUserTravel(id_usuario, id_viagem);
+
+      return res.status(200).json({
+        message: "Apagado",
+        status: "Success",
+      });
+    } catch (err) {
+      console.log(err);
+
+      return res.status(500).json({
+        message: "Bad Request",
+      });
+    }
+  },
+};
+
+export default UserTravelController;
--- app/routes/travel.ts
+++ app/routes/travel.ts
@@ -1,4 +1,5 @@
 import TravelController from "../controllers/travel";
+import UserTravelController from "../controllers/userTravel"
 import { verifyToken } from "../middlewares/auth";
 
 import { Router } from "express";
@@ -7,9 +8,15 @@ const router = Router();
 
 router.post("/new", TravelController.create);
 
+
 router.get("/", TravelController.getTravels);
 router.get("/:id_viagem", TravelController.getDetail);
 router.put("/:id_viagem", TravelController.update);
 router.delete("/:id_viagem", TravelController.delete);
+
+
+router.post("/user/new", UserTravelController.create)
+router.get('/:id_viagem/user', UserTravelController.getDetails)
+router.delete('/:id_viagem/user/:id_usuario', UserTravelController.delete)
 
 export = router;
