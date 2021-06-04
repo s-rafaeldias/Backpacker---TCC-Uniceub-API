dbf5a3641d80afce161c8e03d9873908f542a55d
Rafael Dias <rafa197795@gmail.com>
2021-06-04 16:39:18 -0300

    feat(firebase): adiciona opcao de realizar full delete

--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
 import db from "./app/models";
 import { seedDatabase } from "./app/models/utils";
 import app from "./app/app";
+import { resetFirebase } from './app/utils/firebase';
 
+// Full reset do firebase
+if (process.env.FULL_RESET) {
+  resetFirebase()
+}
 
 if (process.env.ENV === "DEV") {
   db.sync({ force: true }).then(() => {
@@ -9,8 +14,9 @@ if (process.env.ENV === "DEV") {
   });
 } else {
   console.log("Creating DB...");
-  db.sync({ force: true });
+  db.sync({ alter: true });
 }
+
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
